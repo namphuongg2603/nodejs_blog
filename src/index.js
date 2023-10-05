9337630f7ec5a34fd98f57d195fdedae01119ae8
namphuongg2603 <namphuongg2603@gmail.com>
2023-10-05 21:02:22 +0700

    router and controllers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 const handlebars = require ('express-handlebars');
 
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, "public")) );
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
 
 
 //HTTP logger
@@ -24,13 +29,10 @@ app.set('views', path.join(__dirname, 'resource/views') );
 
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//Routes init
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
