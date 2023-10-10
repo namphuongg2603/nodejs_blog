1cceffb8cc32b2b6528d70655d8e2feb1ccbcd2b
namphuongg2603 <namphuongg2603@gmail.com>
2023-10-10 18:50:47 +0700

    install prettier, lind-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,22 +1,13 @@
-
-class NewsController{
-
-
+class NewsController {
     //[GET] /news
     index(req, res) {
-
-            res.render('news');
-          
+        res.render('news');
     }
-
 
     //[GET] /news/:slug
     show(req, res) {
-
         res.send('NEWS DETAIL');
-
     }
-
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
